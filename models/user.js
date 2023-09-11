597dad2a4271d6c04af6def5e716d27684413637
Aditya Singh Rajawat <adityasinghrajawat393@gmail.com>
2023-09-11 22:28:44 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,22 @@
+import { model, Schema, models } from "mongoose";
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: [true, 'Email already exists!'],
+        required: [true, 'Email is required!']
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required!'],
+        unique: true,
+        match: [/^(?=.{8,30}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-30 alphanumeric letters and be unique!"]
+    },
+    image: {
+        type: String
+    },
+});
+
+const User = models.User || model("User", userSchema);
+
+export default User;
