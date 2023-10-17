7b06d40d494f9d3a562b2f4eedf7968d70d5bc4c
Aditya Singh Rajawat <adityasinghrajawat393@gmail.com>
2023-10-17 17:58:24 +0530

    added ? operator to remove fetching error

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -18,8 +18,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
     if (post.creator._id === session?.user.id) return router.push("/profile");
 
-    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+    router.push(`/profile/${post.creator?._id}?name=${post.creator?.username}`);
   };
+
+  console.log(post.creator);
 
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -32,7 +34,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer" onClick={handleProfileClick}>
           <Image
-            src={post.creator.image}
+            src={post.creator?.image}
             alt="user_image"
             width={40}
             height={40}
@@ -41,10 +43,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
-              {post.creator.username}
+              {post.creator?.username}
             </h3>
             <p className="font-inter text-sm text-gray-500">
-              {post.creator.email}
+              {post.creator?.email}
             </p>
           </div>
         </div>
@@ -63,7 +65,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p className="font-inter text-sm blue_gradient cursor-pointer" onClick={() => handleTagClick && handleTagClick(post.tag)}>#{post.tag}</p>
 
-      {session?.user.id === post.creator._id && pathName === '/profile' && (
+      {session?.user.id === post.creator?._id && pathName === '/profile' && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>Edit</p>
           <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>Delete</p>
